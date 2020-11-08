914fbfd6324253cb620490f69815e1fdd9cc84e0
Devita00 <72591183+Devita00@users.noreply.github.com>
2020-11-08 09:52:55 +0700

    Add files via upload

---
+++ Flame_Detection_1.ino
@@ -0,0 +1,72 @@
+const int FLAME_SENSOR_PIN = 2;
+int mtrPin = 3;
+int FLAME_SENSOR_state=0;
+int FLAME_SENSOR_serial_data_R1;
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+void setup()
+{
+  lcd.begin(16,2);
+  Serial.begin(9600);
+  pinMode(FLAME_SENSOR_PIN, INPUT);
+  pinMode(mtrPin, OUTPUT);
+  lcd.setCursor(0,0);
+  lcd.print("FLAME DETECTION");
+  lcd.setCursor(0,1);
+  lcd.print("ROOM 1");
+  delay(1000);
+}
+void loop()
+{
+  FLAME_SENSOR_state = digitalRead(FLAME_SENSOR_PIN);
+  if(FLAME_SENSOR_state== HIGH)
+  {
+    lcd.clear();
+    FLAME_SENSOR_serial_data_R1=10;
+    Serial.write(FLAME_SENSOR_serial_data_R1);
+  }
+  else
+  {
+    lcd.clear();
+    FLAME_SENSOR_serial_data_R1=20;
+    Serial.write(FLAME_SENSOR_serial_data_R1);
+  }
+  
+  if(FLAME_SENSOR_serial_data_R1 == 10)
+  {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("FLAME DETECTED");
+    lcd.setCursor(0,1);
+    lcd.print("ROOM 1");
+    delay(50);
+    digitalWrite(mtrPin, HIGH);
+  }
+  else
+  {
+    if (FLAME_SENSOR_serial_data_R1 == 20)
+    {
+      int FLAME_SENSOR_serial_data_R2= Serial.read();
+      if(FLAME_SENSOR_serial_data_R2 == 30)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("FLAME DETECTED");
+        lcd.setCursor(0,1);
+        lcd.print("ROOM 2");
+        delay(50);
+      }
+      else 
+      {
+        if(FLAME_SENSOR_serial_data_R2 == 40)
+        {
+          lcd.clear();
+          lcd.setCursor(0,1);
+          lcd.print("NO FLAME");
+          delay(50);
+        } 
+      }
+    }
+    digitalWrite(mtrPin, LOW);
+  }
+}
